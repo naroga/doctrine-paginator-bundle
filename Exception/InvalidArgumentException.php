09c0c87d36e8a3c338f760b6e5f8777d454b3731
Pedro Cordeiro <pedro.cordeiro@jadu.co.uk>
2018-05-11 14:17:20 -0300

    Added argument checks to the Paginator signature

---
+++ Exception/InvalidArgumentException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Naroga\DoctrinePaginatorBundle\Exception;
+
+/**
+ * Class InvalidArgumentException
+ * @package Naroga\DoctrinePaginatorBundle\Exception
+ */
+class InvalidArgumentException extends \Exception
+{
+}
--- Paginator.php
+++ Paginator.php
@@ -3,6 +3,8 @@
 namespace Naroga\DoctrinePaginatorBundle;
 
 use Doctrine\ORM\QueryBuilder;
+use Naroga\DoctrinePaginatorBundle\Exception\InvalidArgumentException;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Class Paginator
@@ -13,13 +15,23 @@ class Paginator
     /**
      * Paginates the QueryBuilder
      *
-     * @param QueryBuilder $queryBuilder    A QueryBuilder to be paginated.
-     * @param int $maxResults               Maximum number of results (defaults to 10).
-     * @param int $page                     The page number (defaults to 1).
-     * @return Page                         The paginated resultset.
+     * @param QueryBuilder $queryBuilder                A QueryBuilder to be paginated.
+     * @param int $maxResults                           Maximum number of results (defaults to 10).
+     * @param int $page                                 The page number (defaults to 1).
+     * @return Page                                     The paginated resultset.
+     * @throws \Doctrine\ORM\NonUniqueResultException   In case multiple records get returned on the COUNT(*) query.
+     * @throws InvalidArgumentException                 In case any of the arguments are invalid.
      */
     public function paginate(QueryBuilder $queryBuilder, $page = 1, $maxResults = 10)
     {
+        if (!is_numeric($page) || $page < 1) {
+            throw new InvalidArgumentException("Argument \$page needs to be an integer, grater than 0");
+        }
+
+        if (!is_numeric($maxResults) || $maxResults < 1) {
+            throw new InvalidArgumentException("Argument \$maxResults needs to be an integer, grater than 0");
+        }
+
         if (strtolower($page) == 'all') {
             $items = $queryBuilder->getQuery()->getResult();
             $total = count($items);
